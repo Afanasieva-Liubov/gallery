5d8948076d92c07534193fe0f46f69428d7da9a0
Afanasieva-Liubov <afanasieva.l.d@gmail.com>
2020-11-15 15:38:37 +0300

    Add JpaConfig

---
+++ src/main/java/afanasievald/repository/JpaConfig.java
@@ -0,0 +1,24 @@
+package afanasievald.repository;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public DataSource getDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://${MYSQL_HOST:localhost}:3306/db_example?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("1.3.3.3-,07333f");
+        return dataSourceBuilder.build();
+    }
+}
+/*spring.datasource.url=jdbc:mysql://${MYSQL_HOST:localhost}:3306/db_example?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC
+        spring.datasource.username=root
+        spring.datasource.password=1.3.3.3-,07333f*/
